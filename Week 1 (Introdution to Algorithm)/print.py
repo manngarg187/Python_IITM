76adde7e16a1378c1778afa060f000813515546b
Mann Garg <gargmann963@gmail.com>
2026-03-04 03:13:19 +0530

    Initial Commit

---
+++ Week 1 (Introdution to Algorithm)/print.py
@@ -0,0 +1,12 @@
+print ("My name is Mann Garg.")
+print("Namasta",78, "India")
+a=2
+print(a)
+b=5
+print(b)
+print(a+b)
+print(a*b)
+print("Enter a number:")
+n=int(input())
+print(n*6)
+print(n+8)
